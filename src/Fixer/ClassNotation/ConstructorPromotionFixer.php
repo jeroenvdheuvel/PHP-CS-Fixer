b9c327652f9d34be00a0b598d803c6e79960a9c1
Jeroen van den Heuvel <jeroen.van.den.heuvel@instapro.com>
2021-10-27 17:45:28 +0200

    WIP: Promote properties to constructor arguments

---
+++ src/Fixer/ClassNotation/ConstructorPromotionFixer.php
@@ -0,0 +1,265 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Fixer\ClassNotation;
+
+use InvalidArgumentException;
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Analyzer\FunctionsAnalyzer;
+use PhpCsFixer\Tokenizer\CT;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+use PhpCsFixer\Tokenizer\TokensAnalyzer;
+use RuntimeException;
+
+/**
+ * TODO: Explain here
+ *
+ * @author Jeroen van den Heuvel <jeroenvdheuvel+github@hotmail.com>
+ */
+final class ConstructorPromotionFixer extends AbstractFixer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition(
+            'Promotes class properties to the constructor.',
+            [
+                new CodeSample(
+                    '<?php
+class Foo {
+    public bool $bar;
+    protected int $baz;
+    private $qux;
+
+    public function __construct(bool $bar, int $baz, $qux) {
+        $this->bar = $bar;
+        $this->baz = $baz;
+        $this->qux = $qux;
+    }
+}
+
+class Foo {
+
+    public function __construct(public bool $bar, protected int $baz, private $qux) {
+    }
+}
+'
+                ),
+            ]
+        );
+    }
+
+    protected function applyFix(\SplFileInfo $file, Tokens $tokens): void
+    {
+        // TODO: What about makign `$tokens` a class property instead of passing it along all the time?
+        $classes = $this->extractClassData($tokens);
+
+        foreach (array_reverse($classes) as $class) {
+            $constructorStart = $tokens->getNextTokenOfKind($class['constructor'], ['{']);
+            $constructorEnd = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_CURLY_BRACE, $constructorStart);
+
+            foreach (array_reverse($class['constructorArguments']) as $name => $index) {
+                if (!isset($class['properties'][$name])) {
+                    continue;
+                }
+                $variableAssignmentSequence = $this->extractVariableAssignmentSequence($tokens, $name, $constructorStart, $constructorEnd);
+
+                if (!$variableAssignmentSequence) {
+                    continue;
+                }
+
+                $propertyIndex = $class['properties'][$name];
+                $propertyNextMeaningfulTokenIndex = $tokens->getNextMeaningfulToken($propertyIndex);
+                // TODO: `= null` will not work
+                if ($tokens[$propertyNextMeaningfulTokenIndex]->getContent() !== ';') {
+                    continue;
+                }
+
+                $propertyVisibilityIndex = $this->findPropertyVisibilityIndex($tokens, $propertyIndex);
+                $insertTokens = $this->createPropertyPromotionInsertTokens($tokens, $propertyVisibilityIndex, $propertyIndex);
+
+                $this->clearVariableAssignment(
+                    $tokens,
+                    array_key_first($variableAssignmentSequence),
+                    array_key_last($variableAssignmentSequence)
+                );
+
+                $this->clearClassProperty($tokens, $propertyVisibilityIndex, $propertyNextMeaningfulTokenIndex);
+
+                $tokens->insertAt($index, $insertTokens);
+            }
+        }
+
+        $tokens->clearEmptyTokens();
+    }
+
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isAllTokenKindsFound([T_CLASS, T_FUNCTION, T_VARIABLE]);
+    }
+
+//    /**
+//     * {@inheritdoc}
+//     */
+//    public function isRisky(): bool
+//    {
+//        return true;
+//    }
+    private function extractClassData(Tokens $tokens): array
+    {
+        $tokenAnalyzer= new TokensAnalyzer($tokens);
+        $classyElements = $tokenAnalyzer->getClassyElements();
+
+        $extractedClasses = [];
+        foreach ($classyElements as $index => $classyElement) {
+            $token = $classyElement['token'];
+            $type = $classyElement['type'];
+            $classIndex = $classyElement['classIndex'];
+
+            if ($type === 'property') {
+                $extractedClasses[$classIndex]['properties'][$token->getContent()] = $index;
+                continue;
+            }
+
+            if ($type !== 'method') {
+                continue;
+            }
+
+            $methodIndex = $tokens->getNextMeaningfulToken($index);
+            $methodToken = $tokens[$methodIndex];
+            if ($methodToken->getContent() !== '__construct') {
+                continue;
+            }
+
+            $extractedClasses[$classIndex]['constructor'] = $methodIndex;
+            $extractedClasses[$classIndex]['constructorArguments'] = $this->extractFunctionArguments($tokens, $methodIndex);
+        }
+
+        return $extractedClasses;
+    }
+
+    // TODO: Only enable on php 8
+
+    private function extractFunctionArguments(Tokens $tokens, int $index): array
+    {
+        $functionAnalyzer = new FunctionsAnalyzer();
+        $functionArguments = $functionAnalyzer->getFunctionArguments($tokens, $index);
+
+        $extractedArguments = [];
+
+        foreach ($functionArguments as $argument) {
+            $extractedArguments[$argument->getName()] = $argument->getNameIndex();
+        }
+
+        return $extractedArguments;
+    }
+
+    protected function extractVariableAssignmentSequence(Tokens $tokens, string $variableName, int $from, int $to): ?array
+    {
+        $variableNameWithoutSigil = substr($variableName, 1);
+
+        return $tokens->findSequence(
+            [
+                [T_VARIABLE, '$this'],
+                [T_OBJECT_OPERATOR],
+                [T_STRING, $variableNameWithoutSigil],
+                '=',
+                [T_VARIABLE, $variableName],
+                ';'
+            ],
+            $from,
+            $to
+        );
+    }
+
+    private function createPropertyPromotionInsertTokens(Tokens $tokens, int $visibilityIndex, int $propertyIndex): array
+    {
+        $insertTokens = [];
+
+        for ($i=$visibilityIndex; $i<$propertyIndex; $i++) {
+            try {
+                $insertTokens[] = $this->convertVisibilityToken($tokens[$i]);
+            } catch (InvalidArgumentException $e) {
+                $insertTokens[] = $tokens[$i];
+            }
+        }
+
+        return $insertTokens;
+    }
+
+    private function convertVisibilityToken(Token $token): Token
+    {
+        $convertedId = $this->convertPropertyVisibilityIdToPromotedPropertyId($token->getId());
+
+        return new Token([$convertedId, $token->getContent()]);
+    }
+
+    private function convertPropertyVisibilityIdToPromotedPropertyId(int $id): int
+    {
+        switch ($id) {
+            case T_PUBLIC:
+                return CT::T_CONSTRUCTOR_PROPERTY_PROMOTION_PUBLIC;
+            case T_PROTECTED:
+                return CT::T_CONSTRUCTOR_PROPERTY_PROMOTION_PROTECTED;
+            case T_PRIVATE:
+                return CT::T_CONSTRUCTOR_PROPERTY_PROMOTION_PRIVATE;
+            default:
+                throw new InvalidArgumentException(
+                    sprintf(
+                        'Unsupported it "%d" given, supported ids are: "%s"',
+                        $id,
+                        implode(', ', [T_PUBLIC, T_PROTECTED, T_PRIVATE])
+                    )
+                );
+        }
+    }
+
+    private function findPropertyVisibilityIndex(Tokens $tokens, int $propertyIndex): int
+    {
+        for ($i=$propertyIndex - 1; $i>=$propertyIndex-6; $i--) {
+            if ($tokens[$i]->isGivenKind([T_PUBLIC, T_PROTECTED, T_PRIVATE])) {
+                return $i;
+            }
+        }
+
+        throw new RuntimeException(sprintf(
+            'No visibility (public, protected, private) found for property "%s"',
+            $tokens[$propertyIndex]->getContent()
+        ));
+    }
+
+    private function clearVariableAssignment(Tokens $tokens, int $from, int $to): void
+    {
+        if ($tokens[$from - 1]->isWhitespace()) {
+            $from --;
+        }
+
+        $tokens->clearRange($from, $to);
+    }
+
+    private function clearClassProperty(Tokens $tokens, int $from, int $to): void
+    {
+        if ($tokens[$from - 1]->isWhitespace()) {
+            $from --;
+        }
+
+        $tokens->clearRange($from, $to);
+    }
+}
---
+++ tests/Fixer/ClassNotation/ConstructorPromotionFixerTest.php
@@ -0,0 +1,244 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Fixer\ClassNotation;
+
+use PhpCsFixer\Tests\Test\AbstractFixerTestCase;
+
+/**
+ * @author Jeroen van den Heuvel <jeroenvdheuvel+github@hotmail.com>
+ * @author Miguel Heitor <...>
+ *
+ * @internal
+ *
+ * @covers \PhpCsFixer\Fixer\ClassNotation\ConstructorPromotionFixer
+ */
+final class ConstructorPromotionFixerTest extends AbstractFixerTestCase
+{
+    /**
+     * @dataProvider provideFixCases
+     */
+    public function testFix(string $expected, ?string $input = null): void
+    {
+        $this->doTest($expected, $input);
+    }
+
+    public function provideFixCases(): array
+    {
+        return [
+//            'empty' => [
+//                '',
+//            ],
+            'class with public property on top' => [
+                <<<'PHP'
+<?php
+class Foo {
+
+    public function __construct(public $bar) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    public $bar;
+
+    public function __construct($bar) {
+        $this->bar = $bar;
+    }
+}
+PHP,
+            ],
+            'class with one protected property on top' => [
+                <<<'PHP'
+<?php
+class Foo {
+
+    public function __construct(protected $bar) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    protected $bar;
+
+    public function __construct($bar) {
+        $this->bar = $bar;
+    }
+}
+PHP,
+            ],
+            'class with one private property on top' => [
+                <<<'PHP'
+<?php
+class Foo {
+
+    public function __construct(private $bar) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    private $bar;
+
+    public function __construct($bar) {
+        $this->bar = $bar;
+    }
+}
+PHP,
+            ],
+            'class with one private property on bottom' => [
+                <<<'PHP'
+<?php
+class Foo {
+    public function __construct(private $bar) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    public function __construct($bar) {
+        $this->bar = $bar;
+    }
+
+    private $bar;
+}
+PHP,
+            ],
+            'class with typed property' => [
+                <<<'PHP'
+<?php
+class Foo {
+
+    public function __construct(private bool $bar) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    private bool $bar;
+
+    public function __construct($bar) {
+        $this->bar = $bar;
+    }
+}
+PHP,
+            ],
+            'class with optional typed property' => [
+                <<<'PHP'
+<?php
+class Foo {
+
+    public function __construct(private ?bool $bar) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    private ?bool $bar;
+
+    public function __construct($bar) {
+        $this->bar = $bar;
+    }
+}
+PHP,
+            ],
+            'class with multiple properties' => [
+                <<<'PHP'
+<?php
+class Foo {
+
+    public function __construct(private $bar, protected $baz) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    private $bar;
+
+    protected $baz;
+
+    public function __construct($bar, $baz) {
+        $this->bar = $bar;
+        $this->baz = $baz;
+    }
+}
+PHP,
+            ],
+            'multiple classes with properties' => [
+                <<<'PHP'
+<?php
+class Foo {
+
+    public function __construct(private $bar) {
+    }
+}
+
+class Bar {
+
+    public function __construct(protected $baz) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    private $bar;
+
+    public function __construct($bar) {
+        $this->bar = $bar;
+    }
+}
+
+class Bar {
+    protected $baz;
+
+    public function __construct($baz) {
+        $this->baz = $baz;
+    }
+}
+PHP,
+            ],
+            'class property with docblock' => [
+                <<<'PHP'
+<?php
+class Foo {
+
+    public function __construct(private $bar) {
+    }
+}
+PHP,
+                <<<'PHP'
+<?php
+class Foo {
+    /**
+     * @var bool
+     */
+    private $bar;
+
+    public function __construct($bar) {
+        $this->bar = $bar;
+    }
+}
+PHP,
+            ],
+        ];
+    }
+}
